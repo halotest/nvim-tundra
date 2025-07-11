65bfbcedb9e1380ab0f1a26152d529c70897cda4
Sergei Komarov <halorere@gmail>
2025-07-12 00:37:39 +0400

    updates to indents

--- lua/nvim-tundra/hl_group/plugins/snacks.lua
+++ lua/nvim-tundra/hl_group/plugins/snacks.lua
@@ -4,7 +4,7 @@ M.map = function(ss, opts)
 	local bg_main = ss.bg.normal
 	local bg_float = ss.bg.floating
 	local yellow = "#FFFF00"
-	local red = "#FFFF00"
+	local red = "#000000"
 
 	return {
 		WinSeparator = { fg = bg_main },
@@ -15,10 +15,10 @@ M.map = function(ss, opts)
 		SnacksPickerInputBorder = { link = "DiagnosticInfo" },
 		SnacksPickerBoxBorder = { link = "DiagnosticInfo" },
 		SnacksPickerBorder = { link = "DiagnosticInfo" },
-		SnacksIndent = { link = "Conceal" },
-		-- SnacksPickerBox = { link = "DiagnosticInfo" },
+		SnacksIndent = { fg = ss.cp.gray._800 },
+		SnacksIndentScope = { fg = ss.cp.gray._300 },
 		-- SnacksPicker = { link = "Conceal" },
-		-- SnacksIndent = { link = "Conceal" },
+		-- SnacksPickerBox = { link = "DiagnosticInfo" },
 		-- SnacksPickerInput = { fg = bg_float, bg = bg_float },
 		-- SnacksPickerListBorder = { fg = yellow, bg = yellow },
 	}
